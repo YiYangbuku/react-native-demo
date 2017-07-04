2068ad45c9e333e9cc892f996532d6857756639e
yiyang <Yiyang@thoughtworks.com>
2017-07-04 15:28:36 +0800

    add empty test case

--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -5,8 +5,7 @@
 // // Note: test renderer must be required after react-native.
 // import renderer from 'react-test-renderer';
 //
-// it('renders correctly', () => {
-//   const tree = renderer.create(
-//     <Index />
-//   );
-// });
+it("test case", () => {
+
+});
+
